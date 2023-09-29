23747c04581bf2379c5bffffe8258b08d393cb3e
Thoss24 <t.fogarty24@outlook.com>
2023-09-29 18:46:42 +0100

    lazy loading & deployment to firebase hosting

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,70 +1,92 @@
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
-import EditEventPage from './pages/EditEvent';
-import ErrorPage from './pages/Error';
-import EventDetailPage, {
-  loader as eventDetailLoader,
-  action as deleteEventAction,
-} from './pages/EventDetail';
-import EventsPage, { loader as eventsLoader } from './pages/Events';
-import EventsRootLayout from './pages/EventsRoot';
-import HomePage from './pages/Home';
-import NewEventPage from './pages/NewEvent';
-import RootLayout from './pages/Root';
-import { action as manipulateEventAction } from './components/EventForm';
-import NewsletterPage, { action as newsletterAction } from './pages/Newsletter';
-import AuthenticationPage, { action as authAction } from './pages/Authentication';
+import EditEventPage from "./pages/EditEvent";
+import ErrorPage from "./pages/Error";
+import EventsRootLayout from "./pages/EventsRoot";
+import HomePage from "./pages/Home";
+import NewEventPage from "./pages/NewEvent";
+import RootLayout from "./pages/Root";
+import { action as manipulateEventAction } from "./components/EventForm";
+import AuthenticationPage, {
+  action as authAction,
+} from "./pages/Authentication";
+import { lazy } from "react";
+import { Suspense } from "react";
+
+const EventsPage = lazy(() => import("./pages/Events"));
+const EventDetailPage = lazy(() => import("./pages/EventDetail"));
+const NewsletterPage = lazy(() => import("./pages/Newsletter"));
 
 const router = createBrowserRouter([
   {
-    path: '/',
+    path: "/",
     element: <RootLayout />,
     errorElement: <ErrorPage />,
     children: [
       { index: true, element: <HomePage /> },
       {
-        path: 'events',
+        path: "events",
         element: <EventsRootLayout />,
         children: [
           {
             index: true,
-            element: <EventsPage />,
-            loader: eventsLoader,
+            element: (
+              <Suspense fallback={<p>Loading...</p>}>
+                <EventsPage />
+              </Suspense>
+            ),
+            loader: () =>
+              import("./pages/Events").then((module) => module.loader()),
           },
           {
-            path: ':eventId',
-            id: 'event-detail',
-            loader: eventDetailLoader,
+            path: ":eventId",
+            id: "event-detail",
+            loader: (meta) =>
+              import("./pages/EventDetail").then((module) =>
+                module.loader(meta)
+              ),
             children: [
               {
                 index: true,
-                element: <EventDetailPage />,
-                action: deleteEventAction,
+                element: (
+                  <Suspense fallback={<p>Loading...</p>}>
+                    <EventDetailPage />
+                  </Suspense>
+                ),
+                action: (meta) =>
+                  import("./pages/EventDetail").then((module) =>
+                    module.action(meta)
+                  ),
               },
               {
-                path: 'edit',
+                path: "edit",
                 element: <EditEventPage />,
                 action: manipulateEventAction,
               },
             ],
           },
           {
-            path: 'new',
+            path: "new",
             element: <NewEventPage />,
             action: manipulateEventAction,
           },
         ],
       },
       {
-        path: 'newsletter',
-        element: <NewsletterPage />,
-        action: newsletterAction,
+        path: "newsletter",
+        element: (
+          <Suspense fallback={<p>Loading...</p>}>
+            <NewsletterPage />
+          </Suspense>
+        ),
+        action: (meta) =>
+          import("./pages/Newsletter").then((module) => module.action(meta)),
       },
       {
-        path: 'auth',
+        path: "auth",
         element: <AuthenticationPage />,
-        action: authAction
-      }
+        action: authAction,
+      },
     ],
   },
 ]);
